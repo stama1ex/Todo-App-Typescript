7e1d7996b3dc92c649a694ff36eff0718d7fc458
Alex Stamat <stamalex2000@gmail.com>
2025-07-18 01:20:57 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Todo-App-TypeScript/',
   plugins: [react(), tailwindcss()],
 });
